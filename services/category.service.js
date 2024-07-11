28f3a9aa697c3ba68013f86069c2a5366d3c8e9b
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-11 11:54:12 -0600

    feat: Refactor CategoryService to use Sequelize for CRUD operations

--- services/category.service.js
+++ services/category.service.js
@@ -1,32 +1,39 @@
 const boom = require('@hapi/boom');
+const { models } = require('../lib/sequelize');
 
 class CategoryService {
-
-  constructor(){
-  }
+  constructor() {}
   async create(data) {
-    return data;
+    const newCategory = await models.Category.create(data);
+    return newCategory;
   }
 
   async find() {
-    return [];
+    const categories = await models.Category.findAll();
+    return categories;
   }
 
   async findOne(id) {
-    return { id };
+    const category = await models.Category.findByPk(id, {
+      include: ['products'],
+    });
+    if (!category) {
+      throw boom.notFound('category not found');
+    }
+    return category;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const category = await this.findOne(id);
+    await category.update(changes);
+    return category;
   }
 
   async delete(id) {
-    return { id };
+    const category = await this.findOne(id);
+    await category.destroy();
+    return category;
   }
-
 }
 
 module.exports = CategoryService;
