bc8650adc8c02119507896bd6c99eb060e085e35
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-11 04:55:31 -0600

    feat: Update customer service to use correct sequelize library path

--- services/customer.service.js
+++ services/customer.service.js
@@ -1,5 +1,5 @@
 const boom = require('@hapi/boom');
-const { models } = require('../libs/sequelize');
+const { models } = require('../lib/sequelize');
 
 class CustomerService {
   constructor() {}
@@ -20,9 +20,7 @@ class CustomerService {
   }
 
   async create(data) {
-    const newCustomer = await models.Customer.create(data, {
-      include: ['user'],
-    });
+    const newCustomer = await models.Customer.create(data);
     return newCustomer;
   }
 
