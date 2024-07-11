6732ffe8e02277ff334147129a3bb4589604de56
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-11 11:16:44 -0600

    feat: Add Product model and schema

---
+++ db/models/product.model.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize');
+const { CATEGORY_TABLE } = require('./category.model');
+
+const PRODUCT_TABLE = 'products';
+
+const ProductSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  description: {
+    allowNull: false,
+    type: DataTypes.TEXT,
+  },
+  price: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  image: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  categoryId: {
+    field: 'category_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: CATEGORY_TABLE,
+      key: 'id',
+    },
+  },
+};
+
+class Product extends Model {
+  static associate(models) {
+    this.belongsTo(models.Category, { as: 'category' });
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: PRODUCT_TABLE,
+      modelName: 'Product',
+      timestamps: true,
+    };
+  }
+}
+
+module.exports = { Product, ProductSchema, PRODUCT_TABLE };
