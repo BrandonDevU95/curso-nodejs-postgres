c4dad3172ffa8d44a089d453b3c2f684332fc62a
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-11 12:50:48 -0600

    feat: Create migration for order-product table

---
+++ db/migrations/20240711183201-order-product.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const {
+  ORDER_PRODUCT_TABLE,
+  OrderProductSchema,
+} = require('../models/order-product.model');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema); // Este método crea la tabla en la base de datos con el nombre y el esquema que le pasamos
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable(ORDER_PRODUCT_TABLE); // Este método elimina la tabla de la base de datos
+  },
+};
