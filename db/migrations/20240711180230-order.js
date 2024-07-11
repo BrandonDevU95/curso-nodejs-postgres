9c6f259ebef107d5afc69fa134276b785e7c36a3
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-11 12:50:42 -0600

    refactor: Rename ORDERS_TABLE to ORDER_TABLE in migration file

--- db/migrations/20240711180230-order.js
+++ db/migrations/20240711180230-order.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const { ORDERS_TABLE, OrderSchema } = require('../models/order.model');
+const { ORDER_TABLE, OrderSchema } = require('../models/order.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ORDERS_TABLE, OrderSchema); // Este método crea la tabla en la base de datos con el nombre y el esquema que le pasamos
+    await queryInterface.createTable(ORDER_TABLE, OrderSchema); // Este método crea la tabla en la base de datos con el nombre y el esquema que le pasamos
   },
 
   async down(queryInterface) {
-    await queryInterface.dropTable(ORDERS_TABLE); // Este método elimina la tabla de la base de datos
+    await queryInterface.dropTable(ORDER_TABLE); // Este método elimina la tabla de la base de datos
   },
 };
