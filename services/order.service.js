b7fe964cce3661fad0cfca347d1bc2f0aef456e0
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-11 12:24:43 -0600

    feat: Update OrderService to use Sequelize for CRUD operations

--- services/order.service.js
+++ services/order.service.js
@@ -1,32 +1,45 @@
 const boom = require('@hapi/boom');
+const { models } = require('../lib/sequelize');
 
 class OrderService {
-
-  constructor(){
-  }
+  constructor() {}
   async create(data) {
-    return data;
+    const newOrder = await models.Order.create(data);
+    return newOrder;
   }
 
   async find() {
-    return [];
+    const orders = await models.Order.findAll();
+    return orders;
   }
 
   async findOne(id) {
-    return { id };
+    const order = await models.Order.findByPk(id, {
+      include: [
+        {
+          association: 'customer',
+          include: ['user'],
+        },
+      ],
+    });
+    if (!order) {
+      throw boom.notFound('order not found');
+    }
+    return order;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const order = await models.Order.findByPk(id);
+    if (!order) {
+      throw boom.notFound('order not found');
+    }
+    const updatedOrder = await order.update(changes);
+    return updatedOrder;
   }
 
   async delete(id) {
     return { id };
   }
-
 }
 
 module.exports = OrderService;
