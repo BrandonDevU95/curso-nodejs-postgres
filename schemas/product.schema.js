23ced89ef0d9cb195ea7b8d38efd1d0269a8c1bf
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2024-07-11 19:18:59 -0600

    feat: Add price range query parameters to getProductSchema

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -6,6 +6,8 @@ const price = Joi.number().integer().min(10);
 const description = Joi.string().min(10);
 const image = Joi.string().uri();
 const categoryId = Joi.number().integer();
+const price_min = Joi.number().integer().min(0);
+const price_max = Joi.number().integer().min(0);
 
 const limit = Joi.number().integer().min(1).max(100);
 const offset = Joi.number().integer().min(0);
@@ -33,6 +35,12 @@ const getProductSchema = Joi.object({
 const queryProductSchema = Joi.object({
   limit,
   offset,
+  price,
+  price_min,
+  price_max: price_max.when('price_min', {
+    is: Joi.number().integer(),
+    then: Joi.required(),
+  }),
 });
 
 module.exports = {
